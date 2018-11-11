75e66e693b47c8d17ea3a21ae4e20d374978a7ed
andreybogan <andreybogan@yandex.ru>
2018-11-11 14:38:05 +0400

    Homework 2. Task 1 and 2.

--- global/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DOCUMENT_ROOT", __DIR__ . "/../");
-define("GLOBAL_DIR", DOCUMENT_ROOT . "global/");
-define("FNS_DIR", DOCUMENT_ROOT . "fns/");
-define("WWW_DIR", DOCUMENT_ROOT . "www/");
--- www/index.php
+++ www/index.php
@@ -1,39 +1,52 @@
 <?php
-// Поключаем файлы конфиг и функции.
-include __DIR__ . "/../global/config.php";
-include FNS_DIR . "fns_tree.php";
-?>
+function quickSort($arr)
+{
+    static $step = 0;
+    static $simple = 0;
 
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Дерево каталога</title>
-    <style>
-        li {
-            list-style-type: none;
-            padding: 4px 0;
+    $count = count($arr);
+    if ($count <= 1) {
+        $simple++;
+        return $arr;
+
+    }
+
+    $simple++;
+    $first_val = $arr[0];
+    $simple++;
+    $left_arr = [];
+    $simple++;
+    $right_arr = [];
+
+    for ($i = 1; $i < $count; $i++) {
+        $step++;
+        if ($arr[$i] <= $first_val) {
+            $left_arr[] = $arr[$i];
+        } else {
+            $right_arr[] = $arr[$i];
         }
+    }
 
-        ul {
-            margin-left: 12px;
-            padding-left: 12px;
-        }
-    </style>
-</head>
-<body>
-<h1>Проводник</h1>
+    $simple++;
+    $left_arr = quickSort($left_arr);
 
+    $simple++;
+    $right_arr = quickSort($right_arr);
 
-<?php
-// Получаем путь.
-$path = $_GET['path'] ?? DOCUMENT_ROOT;
-// Печатаем каталог.
-printTree($path);
-?>
+    $simple++;
+    $result = array_merge($left_arr, array($first_val), $right_arr);
 
-</body>
-</html>
+    // Вычисление сложности алгоритма для каждого вызова функции.
+    // В последнем вызове покажется общая сложность алгоритма.
+    if ($step > $simple) {
+        echo "Количесвто шагов: " . ($step + $simple) . "<br>";
+        echo "Сложность элемента: О({$step})<br>";
+    } else {
+        echo "Количесвто шагов: " . ($step + $simple) . "<br>";
+        echo "Сложность элемента: О(" . ($step + $simple) . ")<br>";
+    }
+
+    return $result;
+}
+
+var_dump(quickSort([2, 3, 1, 7, 4, 0, 6, 5, 8, 9, 10, 11, 12]));
